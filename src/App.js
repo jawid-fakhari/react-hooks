702826fc787fd40fa8bab6b858744572938dbd6f
Jawid Fakhari <jawid_fakhari@live.it>
2024-06-15 18:33:12 +0200

    basic of useState hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React, { useState} from 'react';
 import './App.css';
 
 function App() {
+  // hooks non possono essere dentro qualsiasi block per esempio if{} e l'ordine deve essere uguale quando viene chiamato dentro return
+
+  //useState hook
+  // 1- prima di tutto dichiarare il default state che sarebbe useState(), dentro useState mettiamo il valore iniziale
+  // 2- useState hook ci da un array di due valori, che per semplificare lo distructiamo inline direttamente, il 1° valore di array è lo state = il valore corrente di ogni iterazione dentro il nostro render e il 2° valore sarebbe una funzione che aggiorna lo state
+  const [count, setCount] = useState(4)
+
+  // la funzione onclick del button
+  function decrementCount(){
+    //funzione del hook useState che prende il valore corrente e diminuisce di 1
+    // setCount(count - 1)
+    
+    //ma è meglio usare una funzione cosi se dovessimo usare due setCount(), cosi non sovrascriverebbero uno l'altro.
+    setCount(prevCount => prevCount -1);
+    // setCount(prevCount => prevCount -1);
+  }
+  function incrementCount(){
+    setCount (prevCount => prevCount + 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementCount}>+</button>
+    </>
   );
 }
 
