0c68f52ea4ee8abe4672c2d0f4abec476c7bbd6d
Jawid Fakhari <jawid_fakhari@live.it>
2024-06-16 00:12:55 +0200

    wrong way to make an app

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [resourceType, setResourceType] = useState('posts');
-  const [items, setItems] = useState([]);
-
-  //**useEffect hook */
-  // ogni volta che render accade la funzione dentro useEffect si aggiorna
-  // questo puo prendere due parametri, il secondo dentro [state] che costringe useEffect a aggiornarsi solo quando lo state indicato viene cambiato
-
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-      .then((response) => response.json())
-      .then((json) => setItems(json));
-  }, [resourceType]);
-
+//**useMemo hook */
+const [number, setNumber] = useState(0)
+const [dark, setDark] = useState(false)
+const doubleNumber = slowFunction(number)
+const themeStyles = {
+  backgroundColor: dark? "black" : "white",
+  color: dark? "white" : "black"
+}
   return (
     <>
-      <button onClick={() => setResourceType('posts')}>Posts</button>
-      <button onClick={() => setResourceType('users')}>Users</button>
-      <button onClick={() => setResourceType('comments')}>Comments</button>
-      <h1>{resourceType}</h1>
-      {items.map(item => {
-        return <pre>{JSON.stringify(item)}</pre>
-      })}
+      <input type="number" value={number} onChange={e => setNumber(parseInt(e.target.value))}/>
+      <button onClick={() => setDark(prevDark => !prevDark)}>Change Theme</button>
+      <div style={themeStyles}>{doubleNumber}</div>
     </>
   );
+}
+
+function slowFunction(num) {
+  console.log("slowFunction called");
+  for (let i = 0; i < 1000000000; i++) {}
+  return num * 2;
 }
 
 export default App;
