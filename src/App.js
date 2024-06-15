a56c192232d31576976a7242f33ae861a72efdfe
Jawid Fakhari <jawid_fakhari@live.it>
2024-06-15 20:26:56 +0200

    useEffect hook and api

--- src/App.js
+++ src/App.js
@@ -3,22 +3,27 @@ import "./App.css";
 
 function App() {
   const [resourceType, setResourceType] = useState('posts');
+  const [items, setItems] = useState([]);
 
   //**useEffect hook */
   // ogni volta che render accade la funzione dentro useEffect si aggiorna
   // questo puo prendere due parametri, il secondo dentro [state] che costringe useEffect a aggiornarsi solo quando lo state indicato viene cambiato
-  console.log('render');
-  
+
   useEffect(() => {
-    console.log('resource type changed');
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
   }, [resourceType]);
 
   return (
     <>
-      <button onClick={() => setResourceType('post')}>Posts</button>
+      <button onClick={() => setResourceType('posts')}>Posts</button>
       <button onClick={() => setResourceType('users')}>Users</button>
       <button onClick={() => setResourceType('comments')}>Comments</button>
       <h1>{resourceType}</h1>
+      {items.map(item => {
+        return <pre>{JSON.stringify(item)}</pre>
+      })}
     </>
   );
 }
