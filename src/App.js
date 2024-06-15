d5d01327a1110c4dae4dc7b3b7d1978106fa7c7e
Jawid Fakhari <jawid_fakhari@live.it>
2024-06-16 00:31:46 +0200

    useMemo utilizzo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import "./App.css";
 
 function App() {
-//**useMemo hook */
+//**useMemo hook ha due utilizzi più importanti */
 const [number, setNumber] = useState(0)
 const [dark, setDark] = useState(false)
-const doubleNumber = slowFunction(number)
-const themeStyles = {
+// 1- quando abbiamo una funzione lenta, e usando useMemo teniamo in memoria la funzione cosi si evita di ricalcolare ogni volta 
+const doubleNumber = useMemo(() => {return slowFunction(number)
+}, [number]);
+// 2- quando vogliamo sapere se il reference di un oggetto o un array è esattamente era quello l'ultima volta è stato renderizzato e che non faccia render ogni volta che c'è qualche cambiamento
+const themeStyles = useMemo(() => {
+  return {
   backgroundColor: dark? "black" : "white",
   color: dark? "white" : "black"
-}
+  }
+}, [dark]);
+
   return (
     <>
-      <input type="number" value={number} onChange={e => setNumber(parseInt(e.target.value))}/>
+      <input type="number" value={number} onChange={e => setNumber(parseInt(e.target.value))}/><br/>
       <button onClick={() => setDark(prevDark => !prevDark)}>Change Theme</button>
       <div style={themeStyles}>{doubleNumber}</div>
     </>
