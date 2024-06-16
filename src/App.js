ea7a7fedf13569e4c559946a9bd4076fe52157c3
Jawid Fakhari <jawid_fakhari@live.it>
2024-06-16 21:07:15 +0200

    useRef to access DOM element

--- src/App.js
+++ src/App.js
@@ -3,18 +3,28 @@ import React, { useState, useEffect, useRef } from "react";
 function App() {
   //** useRef hook */
   const [name, setName] = useState("");
-  // useRef non causa Re-renders, useRef returns solo un item che sarebbe l'oggetto curret. 
-  const renderCount = useRef(1);
+  //* useRef non causa Re-renders, useRef returns solo un item che sarebbe l'oggetto curret.
+  // const renderCount = useRef(1);
 
-  useEffect(() => {
-    renderCount.current = renderCount.current + 1;
-  });
+  // useEffect(() => {
+  //   renderCount.current = renderCount.current + 1;
+  // });
+
+  //* useRef ci da anche accessibilità al DOM 
+  //In React, we can add a ref attribute to an element to access it directly in the DOM.
+  const inputElement= useRef();
+
+  const focusInput = () => {
+    inputElement.current.focus();
+  }
 
   return (
     <>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
+      {/* <input value={name} onChange={(e) => setName(e.target.value)} />
       <h1>My name is: {name}</h1>
-      <h2>l'app {renderCount.current} volte è stato renderizzato</h2>
+      <h2>l'app {renderCount.current} volte è stato renderizzato</h2> */}
+      <input type="text" ref={inputElement} />
+      <button onClick={focusInput}>Focus Input</button>
     </>
   );
 }
