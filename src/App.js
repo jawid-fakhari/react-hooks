5cbd8fc2be7293c2d446cc677c075115288a92c2
Jawid Fakhari <jawid_fakhari@live.it>
2024-06-16 19:20:13 +0200

    useRef basic

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
-import React, { useState, useMemo } from "react";
-import "./App.css";
+import React, { useState, useEffect, useRef } from "react";
 
 function App() {
-//**useMemo hook ha due utilizzi più importanti */
-const [number, setNumber] = useState(0)
-const [dark, setDark] = useState(false)
-// 1- quando abbiamo una funzione lenta, e usando useMemo teniamo in memoria la funzione cosi si evita di ricalcolare ogni volta 
-const doubleNumber = useMemo(() => {return slowFunction(number)
-}, [number]);
-// 2- quando vogliamo sapere se il reference di un oggetto o un array è esattamente era quello l'ultima volta è stato renderizzato e che non faccia render ogni volta che c'è qualche cambiamento
-const themeStyles = useMemo(() => {
-  return {
-  backgroundColor: dark? "black" : "white",
-  color: dark? "white" : "black"
-  }
-}, [dark]);
+  //** useRef hook */
+  const [name, setName] = useState("");
+  // useRef non causa Re-renders, useRef returns solo un item che sarebbe l'oggetto curret. 
+  const renderCount = useRef(1);
+
+  useEffect(() => {
+    renderCount.current = renderCount.current + 1;
+  });
 
   return (
     <>
-      <input type="number" value={number} onChange={e => setNumber(parseInt(e.target.value))}/><br/>
-      <button onClick={() => setDark(prevDark => !prevDark)}>Change Theme</button>
-      <div style={themeStyles}>{doubleNumber}</div>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <h1>My name is: {name}</h1>
+      <h2>l'app {renderCount.current} volte è stato renderizzato</h2>
     </>
   );
-}
-
-function slowFunction(num) {
-  console.log("slowFunction called");
-  for (let i = 0; i < 1000000000; i++) {}
-  return num * 2;
 }
 
 export default App;
