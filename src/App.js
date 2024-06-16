f27990ea40abf187ff7cee6088954c47bc240716
Jawid Fakhari <jawid_fakhari@live.it>
2024-06-16 21:13:44 +0200

    useRef Tracciare i cambiamenti dello State

--- src/App.js
+++ src/App.js
@@ -2,29 +2,40 @@ import React, { useState, useEffect, useRef } from "react";
 
 function App() {
   //** useRef hook */
-  const [name, setName] = useState("");
   //* useRef non causa Re-renders, useRef returns solo un item che sarebbe l'oggetto curret.
+  // const [name, setName] = useState("");
   // const renderCount = useRef(1);
 
   // useEffect(() => {
   //   renderCount.current = renderCount.current + 1;
   // });
 
-  //* useRef ci da anche accessibilità al DOM 
+  //* useRef ci da anche accessibilità al DOM
   //In React, we can add a ref attribute to an element to access it directly in the DOM.
-  const inputElement= useRef();
+  // const inputElement= useRef();
 
-  const focusInput = () => {
-    inputElement.current.focus();
-  }
+  // const focusInput = () => {
+  //   inputElement.current.focus();
+  // }
+
+  //* useRef ci da possibilità di tracciare i cambiamenti di State.
+  const [inputValue, setInputvalue] = useState("");
+  const prevRenderCount = useRef('');
+
+  useEffect(() => {
+    prevRenderCount.current = inputValue;
+  });
 
   return (
     <>
       {/* <input value={name} onChange={(e) => setName(e.target.value)} />
       <h1>My name is: {name}</h1>
       <h2>l'app {renderCount.current} volte è stato renderizzato</h2> */}
-      <input type="text" ref={inputElement} />
-      <button onClick={focusInput}>Focus Input</button>
+      {/* <input type="text" ref={inputElement} />
+      <button onClick={focusInput}>Focus Input</button> */}
+      <input value={inputValue} onChange={(e) => setInputvalue(e.target.value)} />
+      <h1>My name is: {inputValue}</h1>
+      <h2>Prima mi chiamavo {prevRenderCount.current} </h2>
     </>
   );
 }
